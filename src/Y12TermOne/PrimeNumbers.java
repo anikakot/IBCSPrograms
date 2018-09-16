e1396b9b9089303394a8344d71ab1f4ec91e8bb1
unknown <anika2792001@gmail.com>
2018-09-16 15:35:38 +0700

    whatever poom

--- src/Y12TermOne/PrimeNumbers.java
+++ src/Y12TermOne/PrimeNumbers.java
@@ -4,24 +4,22 @@ public class PrimeNumbers {
 
 	public static void main(String[] args) {
 
-		System.out.print(primesBetween(5, 20));
+		System.out.print(primeFactors(156));
 
 	} // end main
 
-	public static String primesBetween(long N, long N2) { 			//concatenate / find all prime numbers between N and N2
+	public static String primesBetween(long N, long N2) { // concatenate / find all prime numbers between N and N2
 		String output = "";
 		long firstPrime = nextPrime(N);
-		if(firstPrime >= N2) {
+		if (firstPrime >= N2) {
 			return "";
-		}	
-		output = ""+firstPrime;
-		
-		for (long i = firstPrime+1; i < N2; i++) 
-		{
-			if (isPrime(i)) 
-			{
-				output = output +"," + i;				
-			} 
+		}
+		output = "" + firstPrime;
+
+		for (long i = firstPrime + 1; i < N2; i++) {
+			if (isPrime(i)) {
+				output = output + "," + i;
+			}
 		}
 		return output;
 	} // end of function
@@ -42,18 +40,49 @@ public class PrimeNumbers {
 		}
 		return i;
 	} // end of function
-	// this function returns true if N is a prime number and false if N is not a
-	// prime number
+		// this function returns true if N is a prime number and false if N is not a
+		// prime number
+
 	public static boolean isPrime(long N) {
 		if (N <= 1) {
 			return false;
 		}
-		for (long i = 2; i < N; i++) {
+		long squareRootN = ((long) java.lang.Math.sqrt(N)) + 1;
+
+		for (long i = 2; i < squareRootN + 1; i++) {
 			if ((N % i) == 0) {
 				return false;
 			} // end if
 		} // end for loop
 		return true;
+
 	} // end function isPrime ()
 
+	
+	// the method outputs the prime factors of a number
+	
+	public static String primeFactors(long N) { // declares the method name and parameter and a return type  
+				
+		long currentPrimeFactor = 2; // declare the variable and set it to 2
+		String returnString = ""; //declares a string variable and assigns an empty string to it
+		while (N != 1) { // while N is not equal to 1 run this loop 
+			if ((N % currentPrimeFactor) == 0) { // if N is divisible by the current prime factor this if statement, the code inside the body of this if statement will execute
+				N = N / currentPrimeFactor; //this divides N by the current prime factor and sets it to be the new N
+				returnString = returnString + "x" + currentPrimeFactor ; //this appends current prime factor to the return string
+			} // end of if body
+
+			else { //executes if the test expression is false
+				currentPrimeFactor = nextPrime(currentPrimeFactor); //Assigns the next prime number to be the new currentprimefactor 
+			} //end of else body 
+		} //end of while loop 
+		
+		return returnString.substring(1, returnString.length()); //this cuts of the first character of the returnstring and then returns it to the caller 
+
+	} //end of method
+
 } // end of class
+
+/*
+ * find first primes first N primes find prime factors string primeFactors (long
+ * N) decomposition
+ */
